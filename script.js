ccafaf4aabaafc57889f04338bba74cd0abd55ec
gabriel-meiki <gabrielmaeda01@gmail.com>
2024-06-10 14:00:43 -0300

    Finalizando

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ function menuShow() {
         document.querySelector('.icon-nav').src = "assets/menu-close.svg";
     }
 }
+
+function trocarVideo() {
+    var videoFrame = document.getElementById('video-frame');
+    var botaoTexto = document.getElementById('botao-texto');
+
+    if (videoFrame.src.includes('embed/w82TQ44oFGc')) {
+        videoFrame.src = "https://www.youtube.com/embed/0iq5468CiUw?si=6p_LzP6aGAXAJytf";
+        botaoTexto.textContent = "Ver vídeo versão português";
+    } else {
+        videoFrame.src = "https://www.youtube.com/embed/w82TQ44oFGc?si=7YJ5qTqn2VD6LelX";
+        botaoTexto.textContent = "Ver vídeo versão inglês";
+    }
+}
